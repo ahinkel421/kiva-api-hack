7b804e01d29db24f79becfccb082f6be4b4c591f
Adam Hinkel <ahinkel421@gmail.com>
2018-01-16 16:57:59 -0500

    fixed filter by amount remaining

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 let state = {
-	requested: 0,
-	raised: 0
+	requested: -1,
+	raised: -1,
 }
 
-// function getDataFromApi(state) {
-// 	let requestData = {
-// 		format: 'json',
-// 	}
-// }
+function getDataFromApi() {
+	let requestData = {
+		status: 'fundraising',
+		sort_by: 'amount_remaining',
+		per_page: 50
+	}
+	$.ajax({
+		url: 'https://api.kivaws.org/v1/loans/search.json',
+		data: requestData,
+		success: function(result) {
+			let loans = result.loans;
+			for (let i = 0; i < loans.length; i++) {
+				let 
+			}
+			console.log(result.loans);
+		}
+	});
+}
+
 $(document).ready(function() {
 	$('.data').click(function(event) {
 		$(this).html('Check the console!');
-		$.ajax({
-			url: 'https://api.kivaws.org/v1/loans/search.json',
-			data: {
-				status: 'fundraising',
-				sort_by: 'random'
-			},
-			success: function(result) {
-				console.log(result.loans);
-			}
-		});
+		getDataFromApi();
 	});
 });
